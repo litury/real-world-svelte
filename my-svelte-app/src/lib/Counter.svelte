894719c3c536e3e0d068543763616a66d91372e1
litury <litury@icloud.com>
2024-06-21 16:00:23 +0500

    ✨ feat: added counter and update-measuring functionalities

--- my-svelte-app/src/lib/Counter.svelte
+++ my-svelte-app/src/lib/Counter.svelte
@@ -1,3 +1,15 @@
 <script>
+	import {startCount} from '../utils/couter.js';
+	import {createUpdateCounter} from '../utils/update-counter.js';
+
+	const {updateCount, startMeasuring, stopMeasuring} = createUpdateCounter();
+
+	const counter = startCount();
 
 </script>
+
+<button on:click={startMeasuring}>Измерить</button>
+<button on:click={stopMeasuring}>Стоп</button>
+<span>Обновлено {$updateCount} раз</span>
+<span>Считаем {$counter} раз</span>
+
