6dc32f45322f8f83d8777f4200f9a6bd7cdba063
litury <litury@icloud.com>
2024-06-25 15:51:16 +0500

    ✨ feat: added dark mode toggle and imported `ThemeColor.svelte`

--- my-svelte-app/src/App.svelte
+++ my-svelte-app/src/App.svelte
@@ -1,51 +1,22 @@
 <script>
-	const color = 'red';
-	const width = '4rem';
-	const height = '4rem';
+	import ThemeColor from './Chapter02/ThemeColor.svelte';
 
-	let blockColor = true;
+	let darkMode = false;
+
+	function toggleDarkMode() {
+		darkMode = !darkMode;
+	}
 
 </script>
-<div
-		style:background-color={color}
-		style:width={width}
-		style:height={height}
-/>
 
-<div style="
-	background-color: #646cff;
-	width: 4rem;
-	height: 4rem"
-/>
+<div>
+<button on:click={toggleDarkMode}>
+	{darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+</button>
 
-
-
-<div class="{blockColor ? 'test' : 'test2'}"/>
-
-<button class="button" on:click={() => blockColor = !blockColor}/>
-
-{blockColor ? 'Change to white' : 'Change to red'}
-
+<ThemeColor {darkMode}/>
+</div>
 
 <style>
-    .test {
-        background-color: #ff646c;
-        width: 4rem;
-        height: 4rem;
-    }
 
-    .test2 {
-        background-color: #ffffff;
-        width: 4rem;
-        height: 4rem;
-    }
-
-	.button {
-		background-color: #646cff;
-		color: white;
-		border: none;
-		padding:1rem;
-		border-radius: 0.25rem;
-		cursor: pointer;
-	}
 </style>
