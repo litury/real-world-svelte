7cd7e82c3f7497213f9b89630c5997cda29fbe35
litury <litury@icloud.com>
2024-06-25 15:27:07 +0500

    ✨ feat(App.svelte): added dynamic color toggle and styles

--- my-svelte-app/src/App.svelte
+++ my-svelte-app/src/App.svelte
@@ -1,7 +1,51 @@
 <script>
-import Counter from './lib/Counter.svelte';
-import Popup from './Chapter01/02-scroll-blocker/App.svelte';
+	const color = 'red';
+	const width = '4rem';
+	const height = '4rem';
+
+	let blockColor = true;
+
 </script>
+<div
+		style:background-color={color}
+		style:width={width}
+		style:height={height}
+/>
+
+<div style="
+	background-color: #646cff;
+	width: 4rem;
+	height: 4rem"
+/>
 
 
-<Popup/>
+
+<div class="{blockColor ? 'test' : 'test2'}"/>
+
+<button class="button" on:click={() => blockColor = !blockColor}/>
+
+{blockColor ? 'Change to white' : 'Change to red'}
+
+
+<style>
+    .test {
+        background-color: #ff646c;
+        width: 4rem;
+        height: 4rem;
+    }
+
+    .test2 {
+        background-color: #ffffff;
+        width: 4rem;
+        height: 4rem;
+    }
+
+	.button {
+		background-color: #646cff;
+		color: white;
+		border: none;
+		padding:1rem;
+		border-radius: 0.25rem;
+		cursor: pointer;
+	}
+</style>
