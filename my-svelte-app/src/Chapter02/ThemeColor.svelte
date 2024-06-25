b84c162127181745e508bcaf38aeaf8470849ad9
litury <litury@icloud.com>
2024-06-25 15:51:24 +0500

    ✨ feat: added new file `ThemeColor.svelte`

---
+++ my-svelte-app/src/Chapter02/ThemeColor.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let darkMode = false;
+</script>
+
+<body class={darkMode ? 'dark-mode': 'lig-mode'}>
+
+<h1>Hello World</h1>
+</body>
+
+<style>
+    :root {
+        --background-color-light: #ffffff;
+        --text-color-light: #000000;
+        --background-color-dark: #000000;
+        --text-color-dark: #ffffff;
+    }
+
+    .dark-mode {
+        background-color: var(--background-color-light);
+        color: var(--text-color-light);
+    }
+
+    .lig-mode {
+        background-color: var(--background-color-dark);
+        color: var(--text-color-dark);
+    }
+</style>
