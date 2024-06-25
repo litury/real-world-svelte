a48fbc8d562ada3aa17640643f5bbd9c97cfb630
litury <litury@icloud.com>
2024-06-25 15:27:32 +0500

    ✨ feat: added "autoprefixer", "postcss", and "tailwindcss" dependencies

---
+++ my-svelte-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // все файлы с расширением .svelte в папке src
+    "./src/**/*.svelte"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
