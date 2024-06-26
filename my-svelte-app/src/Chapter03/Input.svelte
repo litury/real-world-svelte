e31707b88637b1cd4f2ce7d1afaf25fd7e478c31
litury <litury@icloud.com>
2024-06-26 15:26:42 +0500

    🧹 refactor(Input.svelte): updated component structure

---
+++ my-svelte-app/src/Chapter03/Input.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let name = "John";
+</script>
+
+<input class="input" bind:value={name}/>
+<p>Hello {name}!</p>
+
+<style>
+    .input {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 8px;
+    }
+</style>
